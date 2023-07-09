0883d19867b1cc1e62f2cbd9cbe462bdefa3036d
Mikov Denis <denis.mikov.06@gmail.com>
2023-07-09 10:29:36 +0300

    aftermerge fix

--- GMTK2023/Assets/Scripts/Player/IdlePlayerState.cs
+++ GMTK2023/Assets/Scripts/Player/IdlePlayerState.cs
@@ -1,23 +1,9 @@
 ﻿using System.Linq;
-using UnityEngine;
 
 internal class IdlePlayerState : BasicPlayerState
 {
     private const int InteractionDistance = 2;
 
-    private StateMode currentState;
-    private Block target;
-    private Vector2Int targetPosition;
-
-    public Block Target
-    {
-        get => target;
-        private set
-        {
-            target = value;
-            targetPosition = Vector2Int.FloorToInt(value.transform.position);
-        }
-    }
 
     public IdlePlayerState(PlayerBehavior player) : base(player)
     {
@@ -25,12 +11,12 @@ internal class IdlePlayerState : BasicPlayerState
 
     public override void Update()
     {
-        var workbench = Target.PlacedBlock as WorkbenchBlock;
+        /*var workbench = Target.PlacedBlock as WorkbenchBlock;
         if (!workbench.IsCrafting)
         {
             Player.Inventory.AddItems(workbench.PickItemsUp());
             Player.SwitchState<SortingPlayerState>();
             currentState = StateMode.None;
-        }
+        }*/
     }
 }
